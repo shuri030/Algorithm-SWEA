5520ccd8fec51251ab80b3a90a8b2073c4b14fca
shuri030 <kpo01286@gmail.com>
2018-08-02 02:03:14 +0900

    1940. 가랏! RC카!
    1940. 가랏! RC카!

---
+++ 1940.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int t, N;
+	int c, d;
+
+	scanf("%d", &t);
+
+	for(int tc = 1; tc <= t; tc++) {
+		int m = 0, ans = 0;
+		scanf("%d", &N);
+		for(int i = 1; i <= N; i++) {
+			scanf("%d", &c);
+			if(c == 0) {
+				ans += m;
+			}
+			else if(c == 1) {
+				scanf("%d", &d);
+				m += d;
+				ans += m;
+			}
+			else if(c == 2) {
+				scanf("%d", &d);
+				if(m < d) {
+					m = 0;
+				}
+				else {
+					m -= d;
+				}
+				ans += m;
+			}
+		}
+		printf("#%d %d\n", tc, ans);
+	}
+
+    return 0;
+}
